da63e71f85cc991d66a418da0330ffc6f1fcd282
Yumi yumi <133847607+Vivi13052003@users.noreply.github.com>
2025-09-22 22:09:18 +0700

    Thuy Vi (#9)
    Co-authored-by: Thuy Vi <vivltce170398@fpt.edu.vn>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 /** ⛔⛔⛔   ℂẢℕℍ 𝔹Á𝕆 — ĐỌℂ 𝔽𝕀𝕃𝔼 README 𝕋ℝướℂ 𝕂ℍ𝕀 ℂ𝕆𝔻𝔼   ⛔⛔⛔ */
 
 import { Button } from "@/components/ui/button"
+import { Routes, Route } from 'react-router-dom'
+import PromotionManagement from "./components/ui/Dashboard/PromotionManagement"
+import Dashboard from './components/ui/Dashboard/Dashboard.jsx'
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <Routes>
+      <Route path="/Dashboard" element={<Dashboard />} />
+    </Routes>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
+/** ⛔⛔⛔   ℂẢℕℍ 𝔹Á𝕆 — ĐỌℂ 𝔽𝕀𝕃𝔼 README 𝕋ℝướℂ 𝕂ℍ𝕀 ℂ𝕆𝔻𝔼   ⛔⛔⛔ */
+
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
