56f4c54025dc7d1d7d0024d7ab3369b9c0293dfc
Thúy Vi <vivltce170398@fpt.edu.vn>
2025-09-25 00:11:38 +0700

    ViVLT: Connect api frontend Create and getList promotion (#20)

--- src/services/promotionService.js
+++ src/services/promotionService.js
@@ -1,15 +1,43 @@
-// Vo Lam Thuy Vi
-
+// src/services/promotionService.js
 import API_BASE_URL from "@/config/api";
+
 export async function getPromotionsList() {
-    const res = await fetch(`${API_BASE_URL}/promotions`,{
+    const res = await fetch(`${API_BASE_URL}/promotions/admin`, {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
         },
     });
-     if (!res.ok) {
-    throw new Error("Failed to fetch promotions");
-  }
+
+    console.log("Response status:", res.status);
+
+    if (!res.ok) {
+        const text = await res.text();
+        console.error("Failed response:", text);
+        throw new Error("Failed to fetch promotions");
+    }
+
+    const data = await res.json();
+    console.log("Parsed data:", data);
+    return data;
+}
+
+export async function createPromotion(promo) {
+    const res = await fetch(`${API_BASE_URL}/promotions`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(promo),
+    });
+
+    console.log("Response status:", res.status);
+
+    if (!res.ok) {
+        const text = await res.text();
+        console.error("Failed response:", text);
+        throw new Error("Failed to fetch promotions");
+    }
     return res.json();
 }
+
