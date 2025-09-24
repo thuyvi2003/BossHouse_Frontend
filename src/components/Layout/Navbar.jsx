97446a22a1318c880297e47c8c7ec4fd4a98b24a
nsang280701 <Sangnqce150621@fpt.edu.vn>
2025-09-25 00:13:40 +0700

    update route homepage

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -43,7 +43,7 @@ export default function Navbar() {
             </Link>
 
             <div className="flex gap-8 text-lg font-medium items-center">
-                <NavLink to="/home" className={navLinkClass}>
+                <NavLink to="/" className={navLinkClass}>
                     Home
                 </NavLink>
                 <NavLink to="/products" className={navLinkClass}>
