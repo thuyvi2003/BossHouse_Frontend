e5616e1fd294f4d5a7cd57d69e671ee593a162a2
DESKTOP-55BHRIT\WINDOWS <bonvtce181691@fpt.edu.vn>
2025-09-25 20:36:05 +0700

    Add frontend register + login + forgot password. Also added ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { useEffect } from "react";
+import { useAuthStore } from "@/stores/useAuthStore";
+
 import Dashboard from './pages/DashboardScreen.jsx';
 import Cart from './pages/CartScreen.jsx';
 import HomePage from './pages/HomePage.jsx';
@@ -8,8 +14,20 @@ import Navbar from './components/Layout/Navbar.jsx';
 import Footer from './components/Layout/Footer.jsx';
 import BlogScreen from './pages/BlogScreen.jsx';
 import PostDetail from './pages/PostDetail.jsx';
+import LoginPage from './pages/auth/LoginPage.jsx';
+import RegisterPage from './pages/auth/RegisterPage.jsx';
+import VerifyOtpPage from './pages/auth/VerifyOtpPage.jsx';
+import ForgotPasswordPage from './pages/auth/ForgotPasswordPage.jsx';
+import ResetPasswordPage from './pages/auth/ResetPasswordPage.jsx';
 
 function App() {
+
+  const { checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
@@ -17,6 +35,12 @@ function App() {
       <main className="flex-grow">
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/verify-otp" element={<VerifyOtpPage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+
           <Route path="/products" element={<ProductPage />} />
           <Route path="/product/:id" element={<ProductDetailPage />} />
           <Route path="/Dashboard" element={<Dashboard />} />
@@ -24,6 +48,9 @@ function App() {
           <Route path="/post" element={<BlogScreen />} />
           <Route path="/post/:id" element={<PostDetail />} />
         </Routes>
+
+        <ToastContainer position="top-right" autoClose={5000} />
+
       </main>
       <Footer />
     </div>
