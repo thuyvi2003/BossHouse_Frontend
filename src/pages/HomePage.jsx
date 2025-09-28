c6e5264e29346d03eb4ecc79f320ce064cea464f
VietNguyen77 <vietnguyennguyen30@gmail.com>
2025-09-28 21:46:54 +0700

    update post management and blog, icon notification

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -247,6 +247,7 @@ const HomePage = () => {
                 </div>
             </section>
 
+
             {/* Products Section */}
             <section className="py-16 bg-white">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
