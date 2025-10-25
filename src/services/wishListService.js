be5794e6a8b6aabd6c3e198ecb68244f43e87436
Vo Lam Thuy Vi <vivltce170398@fpt.edu.vn>
2025-10-25 14:23:27 +0700

    ViVLT: Add wishlist group UI (#67)

--- src/services/wishListService.js
+++ src/services/wishListService.js
@@ -10,8 +10,59 @@ export const addToWishlist = async (productVariationId, note) => {
 };
 
 export const getWishlist = async (page = 1, limit = 8) => {
-  const res = await axiosInstance.get(`/wishlists`,{
+  const res = await axiosInstance.get(`/wishlists`, {
     params: { page, limit },
   });
+  return res.data;
+};
+
+export const removeWishlistItem = async (id) => {
+  const res = await axiosInstance.delete(`/wishlists/delete/${id}`);
+  return res.data;
+};
+
+export const clearAllWishlist = async (groupId = null) => {
+  const res = await axiosInstance.delete(`/wishlists/clear`, {
+    params: groupId ? { group_id: groupId } : {},
+  });
+  return res.data;
+};
+
+export const moveToCart = async (id) => {
+  const res = await axiosInstance.patch(`/wishlists/${id}/move-to-cart`);
+  return res.data;
+};
+
+export const moveToGroup = async (id, newGroupId) => {
+  const res = await axiosInstance.patch(`/wishlists/${id}/move-to-group`, {
+    newGroupId,
+  });
+  return res.data;
+};
+
+export const getWishlistGroups = async () => {
+  const res = await axiosInstance.get(`/wishlists/groups`);
+  return res.data;
+};
+
+
+export const createWishlistGroup = async (name, description = "") => {
+  const res = await axiosInstance.post(`/wishlists/groups/create`, {
+    name,
+    description,
+  });
+  return res.data;
+};
+
+
+export const shareWishlistGroup = async (groupId, visibility = "public") => {
+  const res = await axiosInstance.post(`/wishlists/groups/${groupId}/share`, {
+    visibility,
+  });
+  return res.data;
+};
+
+export const disableShare = async (groupId) => {
+  const res = await axiosInstance.patch(`/wishlists/groups/${groupId}/unshare`);
   return res.data;
 };
